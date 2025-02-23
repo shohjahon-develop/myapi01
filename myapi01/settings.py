592a736b718853a0dc5cb63eba9031e737f6ae3d
shohjaxon7 <shohjaxonmamajonov777@gmail.com>
2025-02-22 21:35:29 -0800

    Local changes saved before pulling

--- myapi01/settings.py
+++ myapi01/settings.py
@@ -142,6 +142,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
 
 
 # Default primary key field type
